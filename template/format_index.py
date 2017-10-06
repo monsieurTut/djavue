2c1cd45174f5e3265da35627f90cfac3285f05d4
adrien <derosiere@gmail.com>
2017-10-06 17:28:41 +0200

    Added format_index.py to replace hardcoded staticfiles path with django {% static... syntax.

---
+++ template/format_index.py
@@ -0,0 +1,18 @@
+"""
+Updated this script so manually added css or js files (e.g. from CDN)
+are not replaced with {% static '...' %} Django syntax
+"""
+import re
+
+CONTENT = "{% load staticfiles %}"
+PATH = "templates/index.html"
+FILE = open(PATH, "r")
+for line in FILE:
+    CONTENT += re.sub(r'(href|src)=/static/(css|js)/(app|manifest|vendor)\.(\w+)\.(css|js)',
+                      r'\1="{% static "\2/\3.\4.\5" %}"',
+                      line)
+FILE.close()
+
+FILE = open(PATH, "w")
+FILE.write(CONTENT)
+FILE.close()
